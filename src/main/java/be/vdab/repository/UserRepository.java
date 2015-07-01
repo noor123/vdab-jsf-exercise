5c1cbc6874d19475af5d6f85cd1b6e7cb7a38454
Kevin Van Robbroeck <kevin.vanrobbroeck@gmail.com>
2015-07-01 09:16:31 +0200

    User repository must be transactional :)

--- src/main/java/be/vdab/repository/UserRepository.java
+++ src/main/java/be/vdab/repository/UserRepository.java
@@ -5,9 +5,11 @@ import be.vdab.domain.User;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Named
+@Transactional
 public class UserRepository {
     @PersistenceContext
     private EntityManager entityManager;
@@ -15,4 +17,12 @@ public class UserRepository {
     public List<User> findAll() {
         return entityManager.createQuery("select u from User u", User.class).getResultList();
     }
+
+    public User findById(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    public void save(User user) {
+        entityManager.merge(user);
+}
 }
